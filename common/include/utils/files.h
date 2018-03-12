c814c3ec6a59eab1e18983e99ea7de49dfd4e81a
Sathyam Vellal <sathyam.vellal@gmail.com>
2018-03-12 16:32:14 -0700

    files lib to extract

---
+++ common/include/utils/files.h
@@ -0,0 +1,12 @@
+//
+// Created by Sathyam Vellal on 10/03/2018.
+//
+
+#ifndef LEARNOPENGL_FILES_H
+#define LEARNOPENGL_FILES_H
+
+#include <string>
+
+void extract(std::string& contents, const std::string& filename);
+
+#endif //LEARNOPENGL_FILES_H
---
+++ common/src/utils/files.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Sathyam Vellal on 10/03/2018.
+//
+
+#include <string>
+#include <fstream>
+#include <sstream>
+
+#include "utils/logger.h"
+
+#include "utils/files.h"
+
+void extract(std::string& contents, const std::string& filename)
+{
+    log<LOG_VERBOSE>(L"Opening file %1% ...") % filename.c_str();
+    std::ifstream fileInput;
+    fileInput.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+    fileInput.open(filename);
+    log<LOG_VERBOSE>(L"Reading file to buffer ...");
+    std::stringstream buffer;
+    buffer << fileInput.rdbuf();
+    log<LOG_VERBOSE>(L"Writing buffer to string ...");
+    contents = buffer.str();
+    log<LOG_VERBOSE>(L"Closing file %1% ...") % filename.c_str();
+    fileInput.close();
+}
