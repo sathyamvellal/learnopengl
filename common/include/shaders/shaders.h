237abe5d75346943fdcdc048e85846c64770476f
Sathyam Vellal <sathyam.vellal@gmail.com>
2018-03-15 18:52:08 -0700

    Initial commit for shaders exercise

---
+++ common/include/shaders/shaders.h
@@ -0,0 +1,10 @@
+//
+// Created by Sathyam Vellal on 07/03/2018.
+//
+
+#ifndef LEARNOPENGL_HELLO_TRIANGLE_H
+#define LEARNOPENGL_HELLO_TRIANGLE_H
+
+extern float vertices[];
+
+#endif //LEARNOPENGL_HELLO_TRIANGLE_H
---
+++ src/shaders/shaders.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Sathyam Vellal on 07/03/2018.
+//
+
+#include "shaders/shaders.h"
+
+float vertices[] = {
+        -0.5f, -0.5f,  0.0f,
+        -0.5f,  0.5f,  0.0f,
+         0.5f,  0.5f,  0.0f,
+
+         0.5f,  0.5f,  0.0f,
+         0.5f, -0.5f,  0.0f,
+        -0.5f, -0.5f,  0.0f
+};
