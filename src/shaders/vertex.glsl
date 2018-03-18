3caf8273ac3856b1d7ec518605c8705cb72b8907
Sathyam Vellal <sathyam.vellal@gmail.com>
2018-03-17 19:18:02 -0700

    New uniform "ourColor"

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,10 +1,7 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 
-out vec4 vertexColor;
-
 void main()
 {
     gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);
-    vertexColor = vec4(0.5f, 0.0f, 0.0f, 1.0f);
 }
