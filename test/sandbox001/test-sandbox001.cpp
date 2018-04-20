58011610cd973f33a603c19020105108c2ad7081
Sathyam Vellal <sathyam.vellal@gmail.com>
2018-04-20 10:48:41 -0700

    Updating Sandbox

--- test/sandbox001/test-sandbox001.cpp
+++ test/sandbox001/test-sandbox001.cpp
@@ -14,12 +14,14 @@
 #include "utils/shader.h"
 #include "utils/shapes/circle.hpp"
 #include "utils/shapes/grid.hpp"
+#include "utils/shapes/grid-overlay.hpp"
 
 #include "sandbox001/sandbox001.h"
 
 const unsigned int WIDTH = 800;
 const unsigned int HEIGHT = 800;
 const char *TITLE = "Sandbox 001";
+bool paused = false;
 
 void framebufferSizeCallback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -65,28 +67,39 @@ int main(int argc, char **argv)
     vertexShader.destroy();
     fragmentShader.destroy();
 
-    Grid<double> grid;
     std::vector<Circle<double>> circles;
+    int nCircles = 10;
+    Grid<double> grid(circles);
+    GridOverlay<double> gridOverlay(grid);
 
-    grid.make();
-    grid.drawInit(shaderProgram);
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < nCircles; ++i) {
         circles.emplace_back(Circle<double>(glm::tvec4<double>(signdrand(), signdrand(), 0.0, 1.0), 0.2 * drand()));
         circles[i].make();
         circles[i].drawInit(shaderProgram);
     }
+    grid.make();
+    grid.drawInit(shaderProgram);
+    gridOverlay.make();
+    gridOverlay.drawInit(shaderProgram);
 
     while(!glfwWindowShouldClose(window)) { processInput(window);
 
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        grid.draw();
-        grid.step();
-        for (int i = 0; i < 5; ++i) {
-            circles[i].draw();
-            circles[i].step();
+        if (!paused) {
+            for (int i = 0; i < nCircles; ++i) {
+                circles[i].step();
+            }
+            grid.step();
+            gridOverlay.step();
         }
+
+        for (int i = 0; i < nCircles; ++i) {
+            circles[i].draw();
+        }
+        grid.draw();
+        gridOverlay.draw();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -105,6 +118,7 @@ void framebufferSizeCallback(GLFWwindow *window, int width, int height)
 void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS) {
+        paused = !paused;
     }
 
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
